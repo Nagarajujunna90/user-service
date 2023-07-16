1fb6572cf46cd436eb98e2edd58689cbb3c2414a
njunna <nagarajujunna90@gmail.com>
2023-07-16 16:09:55 +0530

    Implemented Resilience4J

---
+++ src/main/java/com/emandi/user/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.emandi.user.exception;
+
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({ RequestNotPermitted.class })
+    @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+    public ResponseEntity<?> requestNotPermitted(){
+        return new ResponseEntity<>("You have reached time limit",HttpStatus.TOO_MANY_REQUESTS);
+    }
+}
